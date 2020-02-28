763fe5bcb9816baeaae370cbf742006d4d8d51a0
Jairun Diemert <54635185+JairunDiemert@users.noreply.github.com>
2020-02-28 12:50:09 -0600

    Create Menu.h

---
+++ Menu.h
@@ -0,0 +1,23 @@
+//File: Menu.h
+//Author: Jairun Diemert
+//Class: COP3530
+//Project: Class work
+//Description: This is the header file for the Menu class
+#ifndef MENU_H
+#define MENU_H
+
+#include <iostream>
+using namespace std;
+
+class Menu {
+public:
+	
+  void sortMenu(int A[], int SIZE, int low, int high);
+  void buildArray();
+  void mainMenu();
+
+private:
+
+};
+
+#endif 
